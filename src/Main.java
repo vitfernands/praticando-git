c1b6bc71304e6e0f52fd5ab1030927016e8baeed
Vítor Prochnow <161534480+vitfernands@users.noreply.github.com>
2025-06-05 02:02:10 -0300

    commit pelo desktop windows

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Alteracao");
         System.out.println("Alteracao pelo notebook");
+        System.out.println("Alteracao pelo desktop windows");
     }
 }
